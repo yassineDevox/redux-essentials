ffa18cb01ff3cb54b12fd3130ec00aa4a87b177e
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-03-09 18:20:09 +0100

    (Reducer): combine todoReducer with StoreType

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
-
+import StoreTypes from "../constants/store-types";
+import todoReducer from "./todo";
 
 
 
 
 export default combineReducers({
+    [StoreTypes.TODO]: todoReducer
 })
