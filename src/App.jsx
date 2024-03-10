9cd3af3e23e73ea9c08b01c21d33515e358da8ac
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-03-10 12:28:28 +0000

    (App): clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Todos } from "./components/Todos";
 import { Users } from "./components/Users";
 
 
 const App = () => {
   return (
     <div>
-     {/* <Todos/> */}
      <Users/>
     </div>
   )
