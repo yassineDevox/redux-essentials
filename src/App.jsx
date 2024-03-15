4520e9ea03b9fbe0c20edbcef79a87df514cfd6a
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-03-15 01:50:55 +0000

    (UseEffect & cleanup): implement counter with setInterval and cleanInterval

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,23 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom/dist";
-import Home from "./screens/Home";
-import Todos from "./screens/Todos";
-import Users from "./screens/Users";
-import Layout from "./shared/layout";
-import Login from "./screens/Auth/Login";
-import ForgetPass from "./screens/Auth/ForgetPass";
-import ResetPass from "./screens/Auth/ResetPass";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "home",
-        element: <Home />,
-      }, {
-        path: "todos",
-        element: <Todos />,
-      }, {
-        path: "users",
-        element: <Users />,
-      },
-    ]
-  },
-  {
-    path: "/auth",
-    element: <Layout />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-      }, {
-        path: "forget",
-        element: <ForgetPass />,
-      }, {
-        path: "reset",
-        element: <ResetPass />,
-      },
-    ]
-  },
-]);
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [counter, setcounter] = useState(0)
+
+  useEffect(() => {
+    let interval;
+    if (counter < 10)
+      interval = setInterval(() => {
+        console.log('counter:', counter);
+        setcounter(counter + 1);
+      }, 1000)
+    
+    return () => {
+      console.log('cleanup=>', interval)
+      clearInterval(interval)
+    }
+  }, [counter])
   return (
-    <RouterProvider router={router} />
+    <div style={{ textAlign: 'cener' }}>{counter}</div>
   )
 }
 
